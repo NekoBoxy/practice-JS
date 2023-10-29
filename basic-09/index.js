11b8334b5b193711e74572416a54f1e111d07abe
CatBoxy <catboxy@gmail.com>
2023-10-29 23:26:35 +0800

    basic-09 work in progess

--- basic-09/index.js
+++ basic-09/index.js
@@ -1,1 +1,35 @@
 // 1.9 編寫一個名為stars2的函式，功能為按以下模式打印出星星層：
+
+// function stars2(len) {
+//   let word = "";
+//   let jWord = "*";
+//   for (let i = 1; i <= len; i++) {
+//     // word = word + 1;
+//     word += "*";
+//     jWord += "*";
+//     console.log(word);
+//   }
+
+//   for (let j = 1; j <= len - 1; j++) {
+//     jWord = jWord - "*";
+//     console.log(jWord);
+//   }
+// };
+
+function stars2(n) {
+  let word = "";
+  let reverseWord = "";
+  for (let i = 1; i <= n; i++) {
+    word += "*";
+    reverseWord += "*";
+    console.log(word);
+  }
+
+  for (let j = 1; j <= n - 1; j++) {
+    reverseWord
+    console.log("j", j);
+  }
+};
+
+
+stars2(3);
--- basic-09/readme.md
+++ basic-09/readme.md
@@ -1,2 +1,4 @@
 # 說明
 ## 題目：
+## 補充：
+- [處理文字 - JavaScript中的字串](https://developer.mozilla.org/zh-TW/docs/Learn/JavaScript/First_steps/Strings)
