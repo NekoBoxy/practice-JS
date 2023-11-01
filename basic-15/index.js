ea5de39f261fdc6cc6df8e19be4c54cf77792c7a
CatBoxy <catboxy@gmail.com>
2023-11-01 23:49:23 +0800

    basic-15 half

--- basic-15/index.js
+++ basic-15/index.js
@@ -1,1 +1,65 @@
 // 2.5 編寫一個名為pyramid的函式，功能為按以下模式打印出星星層：
+// sss*
+// ss***
+// s*****
+// *******
+
+// hint
+// for 行
+// 	for 顯示s
+// 	for 顯示*
+
+// 用迴圈算行數 * 星數
+function pyramid(n) {
+  let stars = "";
+  // n = 1; stars = "*";
+  // n = 2; stars = ( " " x 3 ) + ( "**" x 1 )
+  // n = 3; stars = ( " " x 2 ) + ( "**" x 2 )
+  // n = 4; stars = ( " " x 1 ) + ( "**" x 3 )
+  // n = 5; stars = ( " " x 0 ) + ( "**" x 4 )
+  for (let i = 1; i <= n; i++) {
+    // 最多為 n 行
+    if (n === 1) {
+      stars = "*";
+      console.log(stars);
+      break;
+    }
+
+    // 算空白， n = 2 ~ 3 
+    for (let j = 3; j <= 2; j--) {
+
+    }
+
+    // 算星數，n > 1
+
+
+  }
+
+
+
+
+
+  // let stars = "*";
+  // if (n === 1) {
+  //   console.log(stars);
+  // }
+  // for (let i = 1; i <= n; i++) {
+  //   for (let j = 1; j <= 3; j++) {
+  //     stars = " " + stars;
+  //     console.log(stars);
+  //     // 
+  //   }
+  //   for (let k = 3; k <= n; k++) {
+  //     stars = stars + "**";
+  //     console.log(stars);
+  //   }
+  // }
+}
+
+// pyramid(1);
+// pyramid(2);
+pyramid(3);
+// pyramid(4);
+
+// console.log("===========");
+// console.log(pyramid(3));
--- basic-15/readme.md
+++ basic-15/readme.md
@@ -1,3 +1,34 @@
 # 說明
 ## 題目：
-## 補充：
+## 補充：function 的 return
+```
+function pyramid(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+pyramid(3);
+console.log("===========");
+console.log(pyramid(3));
+```
+
+結果會是這樣
+1
+2
+3
+===========
+1
+2
+3
+undefined
+
+因為 pyramid(3) 沒有 return 任何東西，預設的回傳值為空，如下
+```
+function pyramid(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+  return;
+}
+```
