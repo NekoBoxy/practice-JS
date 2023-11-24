1c4ed99f44d04cec7d8d4a92175d3ac3772d2167
CatBoxy <catboxy@gmail.com>
2023-11-24 14:00:08 +0800

    basic-16 done

--- basic-16/only-star.js
+++ basic-16/only-star.js
@@ -10,22 +10,22 @@
 // ss***
 // sss*
 
-function inversePyramid(n) {
+function printStars(n) {
   let result = "";
   // 最多 n 層
   for (let i = 1; i <= n; i++) {
-    // 決定空格數量
-    // An = A1 +(n-1)*d
-    // An = 0 + (i-1)*1
-    for (let j = 1; j <= i - 1; j++) {
-      result = result + "s";
-    }
 
-    // 決定星星數量(遞減)
-    // An = A1 + (n-1)*d
-    // An = (n*2-1) + (n-1)*(-2)
-    // for(起始： k = 1 ；總共執行的次數；次數遞增 1){
-    for (let k = 1; k <= ((n * 2 - 1) + (i - 1) * -2); k++) {
+    // 決定星星數量
+    // An = A1 +(n-1)*d
+    // n = 4
+    // A1 = 4*2-1 = 7 ... 第一層
+    // A2 = 3*2-1 = 5 ... 第二層
+    // A3 = 2*2-1 = 3 ... 第三層
+    // A4 = 1*2-1 = 1 ... 第四層
+
+    // 逆印星 pseudo code
+    // for(起始： j = 1 ；總共執行的次數；次數遞增 1){
+    for (let j = 1; j <= ((n * 2 - 1) + (i - 1) * (-2)); j++) {
       result = result + "*";
     }
     result = result + "\n";
@@ -33,4 +33,4 @@ function inversePyramid(n) {
   console.log(result);
 }
 
-inversePyramid(4);
+printStars(4);
